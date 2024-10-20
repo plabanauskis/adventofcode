ce49aef243ec6b8df5a9012c654f8789811cf83b
Paulius Labanauskis <paulius@labanausk.is>
2024-10-20 19:48:04 +0300

    Advent of Code 2017 - day 9

---
+++ src/aoc/y2017/day_9.clj
@@ -0,0 +1,33 @@
+(ns aoc.y2017.day-9)
+
+
+(defn part-1
+  [input]
+  (->> input
+       (reduce
+         (fn [[score current cancel? garbage?] c]
+           (cond
+             cancel? [score current false garbage?]
+             (= \! c) [score current true garbage?]
+             (= \{ c) [score (if garbage? current (inc current)) false garbage?]
+             (= \} c) [(if garbage? score (+ current score)) (if garbage? current (dec current)) false garbage?]
+             (= \< c) [score current false true]
+             (= \> c) [score current false false]
+             :else [score current false garbage?]))
+         [0 0 false false])
+       first))
+
+(defn part-2
+  [input]
+  (->> input
+       (reduce
+         (fn [[n cancel? garbage?] c]
+           (cond
+             cancel? [n false garbage?]
+             (= \! c) [n true garbage?]
+             (and garbage? (not= \> c)) [(inc n) false true]
+             (= \< c) [n false true]
+             (= \> c) [n false false]
+             :else [(+ n (if garbage? 1 0)) false garbage?]))
+         [0 false false])
+       first))
---
+++ test/core_test/y2017/day_9_test.clj
@@ -0,0 +1,25 @@
+(ns core-test.y2017.day-9-test
+  (:require [clojure.test :refer :all]
+            [aoc.y2017.day-9 :as day-9]))
+
+(deftest part-1
+  (testing "1"
+    (is (= 1 (day-9/part-1 "{}")))
+    (is (= 6 (day-9/part-1 "{{{}}}")))
+    (is (= 5 (day-9/part-1 "{{},{}}")))
+    (is (= 16 (day-9/part-1 "{{{},{},{{}}}}")))
+    (is (= 1 (day-9/part-1 "{<a>,<a>,<a>,<a>}")))
+    (is (= 9 (day-9/part-1 "{{<ab>},{<ab>},{<ab>},{<ab>}}")))
+    (is (= 9 (day-9/part-1 "{{<!!>},{<!!>},{<!!>},{<!!>}}")))
+    (is (= 3 (day-9/part-1 "{{<a!>},{<a!>},{<a!>},{<ab>}}")))))
+
+(deftest part-2
+  (testing "2"
+    (is (= 0 (day-9/part-2 "<>")))
+    (is (= 17 (day-9/part-2 "<random characters>")))
+    (is (= 3 (day-9/part-2 "<<<<>")))
+    (is (= 2 (day-9/part-2 "<{!>}>")))
+    (is (= 0 (day-9/part-2 "<!!>")))
+    (is (= 0 (day-9/part-2 "<!!!>>")))
+    (is (= 10 (day-9/part-2 "<{o\"i!a,<{i<a>"))))
+  )
